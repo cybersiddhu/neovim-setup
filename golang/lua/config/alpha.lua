3a93268e65dbb969a8d3b7023b548145c5be3e7f
Siddhartha Basu <biosidd@gmail.com>
2022-12-23 17:26:43 -0600

    feat: add golang configuration

---
+++ golang/lua/config/alpha.lua
@@ -0,0 +1,62 @@
+local M = {}
+
+function M.setup()
+  math.randomseed(os.time())
+  local status_ok, alpha = pcall(require, "alpha")
+  if not status_ok then
+    return
+  end
+  local function pick_color()
+	local colors = {"String", "Identifier", "Keyword", "Number"}
+	return colors[math.random(#colors)]
+  end
+  local function footer()
+    local total_plugins = #vim.tbl_keys(packer_plugins)
+    local datetime = os.date(" %d-%m-%Y   %H:%M:%S")
+    local plugins_text = "   "
+      .. total_plugins
+      .. "  plugins"
+      .. "   v"
+      .. vim.version().major
+      .. "."
+      .. vim.version().minor
+      .. "."
+      .. vim.version().patch
+      .. "   "
+      .. datetime
+
+    -- Quote
+    local fortune = require "alpha.fortune"
+    local quote = table.concat(fortune(), "\n")
+    return plugins_text .. "\n" .. quote
+  end
+
+  local dashboard = require "alpha.themes.dashboard"
+  dashboard.section.header.val = {
+    "                                                     ",
+    "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+    "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+    "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+    "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+    "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+    "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+    "                                                     ",
+  }
+  dashboard.section.header.opts.hl = pick_color()
+  dashboard.section.buttons.val = {
+    dashboard.button("f", "  Find file", ":Telescope find_files theme=ivy<CR>"),
+    dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+    dashboard.button("b", "  New buffer", ":tabnew <CR>"),
+    dashboard.button("r", "  Recently used files", ":Telescope oldfiles theme=ivy<CR>"),
+    dashboard.button("t", "  Find text", ":Telescope live_grep theme=ivy<CR>"),
+    dashboard.button("c", "  Configuration", ":e $MYVIMRC<CR>"),
+    dashboard.button("u", "  Update plugins", ":PackerUpdate<CR>"),
+    dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+  }
+  dashboard.section.buttons.opts.hl = "Keyword"
+  dashboard.section.footer.val = footer()
+  dashboard.section.footer.opts.hl = "Type"
+  dashboard.opts.opts.noautocmd = true
+  alpha.setup(dashboard.opts)
+end
+return M
---
+++ golang/lua/config/octo.lua
@@ -0,0 +1,5 @@
+require("octo").setup({file_panel = {size = 18}})
+local utils = require "utils"
+utils.map("n","<Leader>li","<Cmd>Octo issue list<CR>")
+utils.map("n","<Leader>lp","<Cmd>Octo pr list<CR>")
+utils.map("n","<Leader>lg","<Cmd>Octo gist list<CR>")
