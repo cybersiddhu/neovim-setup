1a570a6d1c6ea0852c9533de41fb1a8f92b65baa
Siddhartha Basu <biosidd@gmail.com>
2023-04-16 04:49:07 -0500

    feat(plugins.lua): add ChatGPT.nvim and backseat.nvim plugins
    The ChatGPT.nvim plugin is added to provide a chatbot interface for the
    user. The backseat.nvim plugin is added to provide a way to save and
    restore the state of the editor.

--- luadev/lua/plugins.lua
+++ luadev/lua/plugins.lua
@@ -176,4 +176,18 @@ return require("packer").startup(function(use)
   }
   --text objects
   use { "wellle/targets.vim" }
+  -- gpt
+  use {
+    "jackMort/ChatGPT.nvim",
+    requires = "MunifTanjim/nui.nvim",
+    config = function()
+      require("chatgpt").setup()
+    end,
+  }
+  use {
+    "james1236/backseat.nvim",
+    config = function()
+      require("backseat").setup()
+    end
+  }
 end)
