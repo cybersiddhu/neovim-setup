194b8ae7aadd32a3646b59beebf6c29c01c114ce
Siddhartha Basu <biosidd@gmail.com>
2023-04-17 08:56:45 -0500

    refactor(neotest.lua): reformat code and improve readability The code has been reformatted to improve readability and consistency. The indentation has been standardized, and the code has been split into logical sections. The mappings have been moved to a separate table to improve readability.

--- typescript/lua/config/neotest.lua
+++ typescript/lua/config/neotest.lua
@@ -1,52 +1,53 @@
 local neotest = require("neotest")
 local utils = require "utils"
 neotest.setup({
-    icons = {
-      running = "↻",
+  icons = {
+    running = "↻",
+  },
+  strategies = {
+    integrated = {
+      width = 180
     },
-    strategies = {
-      integrated = {
-        width = 180
-      },
-    },
-    adapters = {
-      require("neotest-plenary"),
-      require("neotest-jest")({jestCommand = "yarn test --"}),
-    },
-  })
+  },
+  adapters = {
+    require("neotest-plenary"),
+    require("neotest-jest")({ jestCommand = "yarn test --" }),
+    require("neotest-vitest")
+  },
+})
 
-  local mappings = {
-    ["<Leader>nr"] = function()
-      neotest.run.run(vim.fn.expand("%"))
-    end,
-    ["<Leader>ns"] = function()
-      neotest.run.run({ suite = true })
-    end,
-    ["<Leader>nn"] = neotest.run.run,
-    ["<Leader>nd"] = function()
-      neotest.run.run({ strategy = "dap" })
-    end,
-    ["<Leader>nl"] = neotest.run.run_last,
-    ["<Leader>nD"] = function()
-      neotest.run.run_last({ strategy = "dap" })
-    end,
-    ["<Leader>na"] = neotest.run.attach,
-    ["<Leader>no"] = function()
-      neotest.output.open({ enter = true })
-    end,
-    ["<Leader>nO"] = function()
-      neotest.output.open({ enter = true, short = true })
-    end,
-    ["<Leader>np"] = neotest.summary.toggle,
-    ["<Leader>nm"] = neotest.summary.run_marked,
-    ["[n"] = function()
-      neotest.jump.prev({ status = "failed" })
-    end,
-    ["]n"] = function()
-      neotest.jump.next({ status = "failed" })
-    end,
-  }
+local mappings = {
+  ["<Leader>nr"] = function()
+    neotest.run.run(vim.fn.expand("%"))
+  end,
+  ["<Leader>ns"] = function()
+    neotest.run.run({ suite = true })
+  end,
+  ["<Leader>nn"] = neotest.run.run,
+  ["<Leader>nd"] = function()
+    neotest.run.run({ strategy = "dap" })
+  end,
+  ["<Leader>nl"] = neotest.run.run_last,
+  ["<Leader>nD"] = function()
+    neotest.run.run_last({ strategy = "dap" })
+  end,
+  ["<Leader>na"] = neotest.run.attach,
+  ["<Leader>no"] = function()
+    neotest.output.open({ enter = true })
+  end,
+  ["<Leader>nO"] = function()
+    neotest.output.open({ enter = true, short = true })
+  end,
+  ["<Leader>np"] = neotest.summary.toggle,
+  ["<Leader>nm"] = neotest.summary.run_marked,
+  ["[n"] = function()
+    neotest.jump.prev({ status = "failed" })
+  end,
+  ["]n"] = function()
+    neotest.jump.next({ status = "failed" })
+  end,
+}
 
-  for keys, mapping in pairs(mappings) do
-    utils.map("n", keys, "", { callback = mapping })
-  end
+for keys, mapping in pairs(mappings) do
+  utils.map("n", keys, "", { callback = mapping })
+end
