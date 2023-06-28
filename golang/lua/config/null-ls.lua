7336e969171795ac3added33834169dba48cfc9f
Siddhartha Basu <biosidd@gmail.com>
2023-06-28 18:26:10 -0500

    chore(null-ls.lua): reformat code for better readability and consistency
    The code in null-ls.lua has been reformatted to improve readability and
    consistency. The changes include adjusting indentation and adding spaces
    around curly braces for better code style.

--- golang/lua/config/null-ls.lua
+++ golang/lua/config/null-ls.lua
@@ -1,7 +1,7 @@
 local nls = require "null-ls"
-local sources = { 
-	nls.builtins.formatting.golines.with({
-		extra_args = { "-m", "80" },
-	}), 
+local sources = {
+  nls.builtins.formatting.golines.with({
+    extra_args = { "-m", "80" },
+  }),
 }
-nls.setup({sources = sources})
+nls.setup({ sources = sources })
