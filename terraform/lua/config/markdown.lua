2dafe8a8c452963b926f95a44a64549110347c80
Siddhartha Basu <biosidd@gmail.com>
2023-08-09 13:07:12 -0500

    feat(markdown.lua): add mapping for stopping Markdown preview and enable preview URL echo
    The mapping "<Leader>ms" is added to stop the Markdown preview. This
    allows users to easily stop the preview when they are done viewing it.
    Additionally, the option vim.g.mkdp_echo_preview_url is set to 1,
    enabling the echo of the preview URL. This provides users with the URL
    of the previewed Markdown file, making it easier for them to share or
    access the previewed content.

--- terraform/lua/config/markdown.lua
+++ terraform/lua/config/markdown.lua
@@ -1,3 +1,4 @@
 local utils = require "utils"
 utils.map("n","<Leader>mp","<Plug>MarkdownPreview", {noremap = false, silent = true})
-vim.g.mkdp_refresh_slow = 1
+utils.map("n","<Leader>ms","<Plug>MarkdownPreviewStop", {noremap = false, silent = true})
+vim.g.mkdp_echo_preview_url = 1
