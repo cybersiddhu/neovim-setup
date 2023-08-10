36dbfb4d18e3d6dd0fb24c4cd923ee8848c621b3
Siddhartha Basu <biosidd@gmail.com>
2023-08-09 20:12:32 -0500

    refactor(nvim-tree.lua): reorganize and improve key mappings, add custom on_attach function
    The key mappings in the nvim-tree.lua file have been reorganized and
    improved for better readability and consistency. A custom on_attach
    function has been added to handle specific key mappings and actions.
    Support for live filtering and dotfiles has been added, allowing for
    more control over the displayed files and folders. The view settings
    have been adjusted to set a specific width for the nvim-tree window. The
    renderer options have been modified to group empty folders.

--- python/lua/config/nvim-tree.lua
+++ python/lua/config/nvim-tree.lua
@@ -1,12 +1,54 @@
-require("nvim-tree").setup{
-	disable_netrw = true,
-	hijack_netrw = true,
-	filters = {
-		custom = {".git"},
-	},
-}
+local function on_attach(bufnr)
+  local api = require "nvim-tree.api"
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+  local function edit_or_open()
+    local node = api.tree.get_node_under_cursor()
+    if node.nodes ~= nil then
+      api.node.open.edit()
+    else
+      api.node.open.edit()
+      api.tree.close()
+    end
+  end
+  local function vsplit_preview()
+    local node = api.tree.get_node_under_cursor()
+    if node.nodes ~= nil then
+      api.node.open.edit()
+    else
+      api.node.open.vertical()
+    end
+    api.tree.focus()
+  end
+  api.config.mappings.default_on_attach(bufnr)
+  -- custom keymaps
+  vim.keymap.set("n", "l", edit_or_open, opts("Edit Or Open"))
+  vim.keymap.set("n", "L", vsplit_preview, opts("Vsplit Preview"))
+  vim.keymap.set("n", "h", api.tree.close, opts("Close"))
+  vim.keymap.set("n", "H", api.tree.collapse_all, opts("Collapse All"))
+end
+
+require("nvim-tree").setup({
+  on_attach = on_attach,
+  sort_by = "case_sensitive",
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+  live_filter = {
+    prefix = "[FILTER]: ",
+    always_show_folders = false, -- Turn into false from true by default
+  }
+})
+
 local utils = require "utils"
-utils.map("n","<Leader>e","<Cmd>NvimTreeToggle<CR>")
-utils.map("n","<Leader>r","<Cmd>NvimTreeRefresh<CR>")
-utils.map("n","<Leader>nd","<Cmd>NvimTreeFindFile<CR>")
-utils.map("n","<Leader>no","<Cmd>NvimTreeFocus<CR>")
+utils.map("n", "<Leader>e", "<Cmd>NvimTreeToggle<CR>")
+utils.map("n", "<Leader>r", "<Cmd>NvimTreeRefresh<CR>")
+utils.map("n", "<Leader>nd", "<Cmd>NvimTreeFindFile<CR>")
+utils.map("n", "<Leader>no", "<Cmd>NvimTreeFocus<CR>")
