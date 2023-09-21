85b1aeae00089c34737e2c73d6f87eb2d6a4ae42
Siddhartha Basu <biosidd@gmail.com>
2023-09-21 07:21:22 -0500

    chore(neogen.lua): remove neogen.lua configuration file
    The neogen.lua configuration file has been removed. This file was no
    longer needed and was deleted to clean up the project directory.

--- golang/lua/config/neogen.lua
+++
@@ -1,4 +0,0 @@
-require("neogen").setup({snippet_engine = "luasnip"})
-local utils = require "utils"
-utils.map("n", "<Leader>nf", ":lua require('neogen').generate({type = 'func'})<CR>")
-utils.map("n", "<Leader>nt", ":lua require('neogen').generate({type = 'type'})<CR>")
