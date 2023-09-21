d36c393dd7b87feb2480c9efc6b6c99793fa717e
Siddhartha Basu <biosidd@gmail.com>
2023-09-21 07:12:19 -0500

    chore(plugins.lua): update plugin references to use hiphish/rainbow-delimiters.nvim
    The plugin references in both golang/lua/plugins.lua and
    typescript/lua/plugins.lua have been updated to use
    hiphish/rainbow-delimiters.nvim instead of p00f/nvim-ts-rainbow. This
    change was made to switch to a more actively maintained plugin for
    better functionality and support.

--- golang/lua/plugins.lua
+++ golang/lua/plugins.lua
@@ -8,7 +8,7 @@ return require("packer").startup(function(use)
     { "nvim-treesitter/nvim-treesitter-refactor",    after = "nvim-treesitter-textobjects" },
     { "windwp/nvim-ts-autotag",                      after = "nvim-treesitter-refactor" },
     {
-      "p00f/nvim-ts-rainbow",
+      "hiphish/rainbow-delimiters.nvim",
       after = "nvim-ts-autotag",
       config = function()
         require("config.treesitter")
