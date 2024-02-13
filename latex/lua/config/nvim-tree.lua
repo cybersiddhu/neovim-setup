f69137b199c8cfe412be60dc2b7afc25ca4000d6
Siddhartha Basu <biosidd@gmail.com>
2024-02-12 20:07:52 -0600

    feat(nvim-tree.lua): enhance nvim-tree configuration for better usability and functionality
    The nvim-tree configuration has been significantly enhanced. The changes
    include the addition of custom keymaps for editing or opening files,
    closing the tree, and collapsing all nodes. The diagnostics feature has
    been enabled to provide better error detection. The 'modified' feature
    has been enabled to highlight modified files. The tree now syncs with
    the current working directory and respects buffer's current working
    directory. The tree view width has been set to 30 for better visibility.
    The renderer has been configured to add trailing characters, group empty
    directories, highlight modified and opened files, and highlight git
    changes. The live filter has been configured with a custom prefix and
    set to not always show folders. These changes provide a more intuitive
    and feature-rich user experience when navigating the project structure.

--- latex/lua/config/nvim-tree.lua
+++ latex/lua/config/nvim-tree.lua
@@ -1,12 +1,64 @@
-require("nvim-tree").setup {
-  disable_netrw = true,
-  hijack_netrw = true,
-  filters = {
-    custom = { ".git" },
+local function on_attach(bufnr)
+  local api = require "nvim-tree.api"
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+  local function edit_or_open()
+    local node = api.tree.get_node_under_cursor()
+    if node.nodes ~= nil then
+      api.node.open.edit()
+    else
+      api.node.open.edit()
+      api.tree.close()
+    end
+  end
+  local function vsplit_preview()
+    local node = api.tree.get_node_under_cursor()
+    if node.nodes ~= nil then
+      api.node.open.edit()
+    else
+      api.node.open.vertical()
+    end
+    api.tree.focus()
+  end
+  api.config.mappings.default_on_attach(bufnr)
+  -- custom keymaps
+  vim.keymap.set("n", "l", edit_or_open, opts("Edit Or Open"))
+  vim.keymap.set("n", "L", vsplit_preview, opts("Vsplit Preview"))
+  vim.keymap.set("n", "h", api.tree.close, opts("Close"))
+  vim.keymap.set("n", "H", api.tree.collapse_all, opts("Collapse All"))
+end
+
+require("nvim-tree").setup({
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true
   },
-}
+  modified = {
+    enable = true
+  },
+  sync_root_with_cwd = true,
+  respect_buf_cwd = true,
+  on_attach = on_attach,
+  sort_by = "case_sensitive",
+  view = {
+    width = 30,
+  },
+  renderer = {
+    add_trailing = true,
+    group_empty = true,
+    highlight_modified = "icon",
+    highlight_opened_files = "icon",
+    highlight_git = true
+  },
+  live_filter = {
+    prefix = "[FILTER]: ",
+    always_show_folders = false, -- Turn into false from true by default
+  }
+})
+
 local utils = require "utils"
 utils.map("n", "<Leader>e", "<Cmd>NvimTreeToggle<CR>")
 utils.map("n", "<Leader>r", "<Cmd>NvimTreeRefresh<CR>")
-utils.map("n", "<Leader>nf", "<Cmd>NvimTreeFindFile<CR>")
+utils.map("n", "<Leader>nd", "<Cmd>NvimTreeFindFile<CR>")
 utils.map("n", "<Leader>no", "<Cmd>NvimTreeFocus<CR>")
