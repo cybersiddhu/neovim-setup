0ab007f748047096197d4efa7e612a6bb0a22526
Siddhartha Basu <biosidd@gmail.com>
2024-02-19 19:55:47 -0600

    feat(plugins.lua): add 'martineausimon/nvim-lilypond-suite' plugin for LilyPond support
    The 'martineausimon/nvim-lilypond-suite' plugin is added to provide
    support for LilyPond files. This plugin is configured to use 'skimpdf'
    as the PDF viewer for both LilyPond and LaTeX files. An autocmd is also
    added to sync syntax from the start of the buffer whenever a LilyPond
    file is opened. This change enhances the functionality of the editor by
    providing support for an additional file type.

--- latex/lua/plugins.lua
+++ latex/lua/plugins.lua
@@ -1,149 +1,169 @@
 -- plugin installation using packer
 return require("packer").startup(function(use)
-	use "wbthomason/packer.nvim" -- works
-	-- treesitter
-	use {
-		{ "nvim-treesitter/nvim-treesitter",             run = { ":TSUpdate" } },
-		{ "nvim-treesitter/nvim-treesitter-textobjects", after = "nvim-treesitter" },
-		{ "nvim-treesitter/nvim-treesitter-refactor",    after = "nvim-treesitter-textobjects" },
-		{ "windwp/nvim-ts-autotag",                      after = "nvim-treesitter-refactor" },
-		{
-			"hiphish/rainbow-delimiters.nvim",
-			after = "nvim-ts-autotag",
-			config = function()
-				require("config.treesitter")
-				require("config.folding")
-			end
-		},
-		{ "JoosepAlviste/nvim-ts-context-commentstring",
-			requires = { "numToStr/Comment.nvim" },
-			config = [[require("config.comment")]],
-		},
-		{ "nvim-treesitter/nvim-treesitter-context",
-			after = "nvim-ts-context-commentstring",
-			config = [[require("config.treesitter_context")]],
-		}, -- works
-		{ "shaunsingh/nord.nvim",
-			event = "VimEnter",
-			config = "vim.cmd[[colorscheme nord]]",
-		},
-		{ "stevearc/aerial.nvim",
-			config = [[require("config.aerial")]],
-		},
-	}
-	-- lsp and snipetts
-	use {
-		{ "b0o/schemastore.nvim" },
-		{ "hrsh7th/nvim-cmp" },
-		{ "hrsh7th/cmp-nvim-lsp",
-			requires = {
-				{
-					"neovim/nvim-lspconfig",
-					config = [[require("config.lsp")]],
-				}
-			}
-		},
-		{ "hrsh7th/cmp-buffer" }, -- works
-		{ "f3fora/cmp-spell" }, -- works,
-		{ "hrsh7th/cmp-calc" },
-		{ "hrsh7th/cmp-path" },
-		{ "hrsh7th/cmp-nvim-lsp-document-symbol" },
-		{ "hrsh7th/cmp-cmdline" },
-		{ "octaltree/cmp-look" },
-		{ "ray-x/cmp-treesitter" },
-		{ "onsails/lspkind-nvim" }, -- works
-		{ "L3MON4D3/LuaSnip",
-			requires = {
-				{ "saadparwaiz1/cmp_luasnip" },
-				{ "rafamadriz/friendly-snippets" },
-			},
-			config = function()
-				require("config.autocomplete")
-				require("luasnip.loaders.from_vscode").load()
-			end,
-		}, -- works
-	}
-	-- telescope
-	use {
-		{ "nvim-telescope/telescope.nvim",             requires = { "nvim-lua/plenary.nvim" } },
-		{ "nvim-telescope/telescope-file-browser.nvim" },
-		{ "nvim-telescope/telescope-fzf-native.nvim",
-			run = "make",
-			config = [[require("config.telescope")]],
-		},
-		{ "nvim-telescope/telescope-symbols.nvim" },
-	}
-	-- tabline
-	use { "romgrk/barbar.nvim",
-		requires = { "nvim-tree/nvim-web-devicons" },
-		config = [[require("config.tabline")]],
-	}
-	-- git
-	use {
-		{ "lewis6991/gitsigns.nvim",
-			config = function()
-				require("gitsigns").setup({ current_line_blame = true })
-				vim.cmd [[highlight default link GitSignsCurrentLineBlame LineNr]]
-				require("utils").map("n", "<Leader>cb", "<Cmd>Gitsigns toggle_current_line_blame<CR>")
-			end
-		},
-		{ "rhysd/git-messenger.vim" },
-		{ "pwntester/octo.nvim",
-			config = [[require("config.octo")]],
-		},
-	}
-	-- hop
-	use { "phaazon/hop.nvim", config = [[require("config.hop")]] }
-	-- notify
-	use { "rcarriga/nvim-notify",
-		event = "VimEnter",
-		config = function()
-			vim.notify = require("notify")
-		end,
-	}
-	-- statusline
-	use { "nvim-lualine/lualine.nvim",
-		event = "VimEnter",
-		config = [[require("config.lualine")]],
-	}
-	-- terminal
-	use { "voldikss/vim-floaterm",
-		event = "VimEnter",
-		config = [[require("config.floaterm")]],
-	}
-	-- editor configuration
-	use { "editorconfig/editorconfig-vim" }
-	-- autopair
-	use { "windwp/nvim-autopairs",
-		config = function()
-			require('nvim-autopairs').setup {}
-		end
-	}
-	-- startup screen
-	use { "goolord/alpha-nvim",
-		config = function()
-			require("config.alpha").setup()
-		end,
-	}
-	-- file explorer
-	use { "nvim-tree/nvim-tree.lua",
-		config = function()
-			require("config.nvim-tree")
-		end,
-	}
-	use { "elihunter173/dirbuf.nvim" }
-	-- semantic highlights
-	use { "m-demare/hlargs.nvim",
-		config = function()
-			require("hlargs").setup()
-		end,
-	}
-	-- gpt
-	use({
-		"robitx/gp.nvim",
-		config = function()
-			require("config.gp")
-		end,
-	})
-	use { "wellle/targets.vim" }
+  use "wbthomason/packer.nvim" -- works
+  -- treesitter
+  use {
+    { "nvim-treesitter/nvim-treesitter",             run = { ":TSUpdate" } },
+    { "nvim-treesitter/nvim-treesitter-textobjects", after = "nvim-treesitter" },
+    { "nvim-treesitter/nvim-treesitter-refactor",    after = "nvim-treesitter-textobjects" },
+    { "windwp/nvim-ts-autotag",                      after = "nvim-treesitter-refactor" },
+    {
+      "hiphish/rainbow-delimiters.nvim",
+      after = "nvim-ts-autotag",
+      config = function()
+        require("config.treesitter")
+        require("config.folding")
+      end
+    },
+    { "JoosepAlviste/nvim-ts-context-commentstring",
+      requires = { "numToStr/Comment.nvim" },
+      config = [[require("config.comment")]],
+    },
+    { "nvim-treesitter/nvim-treesitter-context",
+      after = "nvim-ts-context-commentstring",
+      config = [[require("config.treesitter_context")]],
+    }, -- works
+    { "shaunsingh/nord.nvim",
+      event = "VimEnter",
+      config = "vim.cmd[[colorscheme nord]]",
+    },
+    { "stevearc/aerial.nvim",
+      config = [[require("config.aerial")]],
+    },
+  }
+  -- lsp and snipetts
+  use {
+    { "b0o/schemastore.nvim" },
+    { "hrsh7th/nvim-cmp" },
+    { "hrsh7th/cmp-nvim-lsp",
+      requires = {
+        {
+          "neovim/nvim-lspconfig",
+          config = [[require("config.lsp")]],
+        }
+      }
+    },
+    { "hrsh7th/cmp-buffer" }, -- works
+    { "f3fora/cmp-spell" },   -- works,
+    { "hrsh7th/cmp-calc" },
+    { "hrsh7th/cmp-path" },
+    { "hrsh7th/cmp-nvim-lsp-document-symbol" },
+    { "hrsh7th/cmp-cmdline" },
+    { "octaltree/cmp-look" },
+    { "ray-x/cmp-treesitter" },
+    { "onsails/lspkind-nvim" }, -- works
+    { "L3MON4D3/LuaSnip",
+      requires = {
+        { "saadparwaiz1/cmp_luasnip" },
+        { "rafamadriz/friendly-snippets" },
+      },
+      config = function()
+        require("config.autocomplete")
+        require("luasnip.loaders.from_vscode").load()
+      end,
+    }, -- works
+  }
+  -- telescope
+  use {
+    { "nvim-telescope/telescope.nvim",             requires = { "nvim-lua/plenary.nvim" } },
+    { "nvim-telescope/telescope-file-browser.nvim" },
+    { "nvim-telescope/telescope-fzf-native.nvim",
+      run = "make",
+      config = [[require("config.telescope")]],
+    },
+    { "nvim-telescope/telescope-symbols.nvim" },
+  }
+  -- tabline
+  use { "romgrk/barbar.nvim",
+    requires = { "nvim-tree/nvim-web-devicons" },
+    config = [[require("config.tabline")]],
+  }
+  -- git
+  use {
+    { "lewis6991/gitsigns.nvim",
+      config = function()
+        require("gitsigns").setup({ current_line_blame = true })
+        vim.cmd [[highlight default link GitSignsCurrentLineBlame LineNr]]
+        require("utils").map("n", "<Leader>cb", "<Cmd>Gitsigns toggle_current_line_blame<CR>")
+      end
+    },
+    { "rhysd/git-messenger.vim" },
+    { "pwntester/octo.nvim",
+      config = [[require("config.octo")]],
+    },
+  }
+  -- hop
+  use { "phaazon/hop.nvim", config = [[require("config.hop")]] }
+  -- notify
+  use { "rcarriga/nvim-notify",
+    event = "VimEnter",
+    config = function()
+      vim.notify = require("notify")
+    end,
+  }
+  -- statusline
+  use { "nvim-lualine/lualine.nvim",
+    event = "VimEnter",
+    config = [[require("config.lualine")]],
+  }
+  -- terminal
+  use { "voldikss/vim-floaterm",
+    event = "VimEnter",
+    config = [[require("config.floaterm")]],
+  }
+  -- editor configuration
+  use { "editorconfig/editorconfig-vim" }
+  -- autopair
+  use { "windwp/nvim-autopairs",
+    config = function()
+      require('nvim-autopairs').setup {}
+    end
+  }
+  -- startup screen
+  use { "goolord/alpha-nvim",
+    config = function()
+      require("config.alpha").setup()
+    end,
+  }
+  -- file explorer
+  use { "nvim-tree/nvim-tree.lua",
+    config = function()
+      require("config.nvim-tree")
+    end,
+  }
+  use { "elihunter173/dirbuf.nvim" }
+  -- semantic highlights
+  use { "m-demare/hlargs.nvim",
+    config = function()
+      require("hlargs").setup()
+    end,
+  }
+  -- gpt
+  use({
+    "robitx/gp.nvim",
+    config = function()
+      require("config.gp")
+    end,
+  })
+  use { "wellle/targets.vim" }
+  use { "martineausimon/nvim-lilypond-suite",
+    config = function()
+      require("nvls").setup({
+        lilypond = {
+          options = {
+            pdf_viewer = "skimpdf",
+          },
+        },
+        latex = {
+          options = {
+            pdf_viewer = "skimpdf",
+          },
+        },
+      })
+      vim.api.nvim_create_autocmd('BufEnter', {
+        command = "syntax sync fromstart",
+        pattern = { "*.ly", "*.ily", }
+      })
+    end
+  }
 end)
