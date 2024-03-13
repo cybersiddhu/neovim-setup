6fcc9f8b1a77eb8d71282ef0e194e05c476c59d3
Siddhartha Basu <biosidd@gmail.com>
2024-03-13 09:08:36 -0500

    feat(gp.lua): update AI model from 'gpt-4-1106-preview' to 'gpt-4-turbo-preview'
    The AI model has been updated to 'gpt-4-turbo-preview' to leverage the
    latest advancements in AI technology. This change is expected to improve
    the performance and efficiency of the AI assistant and code editor.

--- golang/lua/config/gp.lua
+++ golang/lua/config/gp.lua
@@ -16,7 +16,7 @@ require("gp").setup({
       chat = true,
       command = true,
       -- string with model name or table with model name and parameters
-      model = { model = "gpt-4-1106-preview", temperature = 1.1, top_p = 1 },
+      model = { model = "gpt-4-turbo-preview", temperature = 1.1, top_p = 1 },
       -- system prompt (use this to specify the persona/role of the AI)
       system_prompt = "You are a general AI assistant.\n\n"
           .. "The user provided the additional info about how they would like you to respond:\n\n"
@@ -33,7 +33,7 @@ require("gp").setup({
       chat = false,
       command = true,
       -- string with model name or table with model name and parameters
-      model = { model = "gpt-4-1106-preview", temperature = 0.8, top_p = 1 },
+      model = { model = "gpt-4-turbo-preview", temperature = 0.8, top_p = 1 },
       -- system prompt (use this to specify the persona/role of the AI)
       system_prompt = "You are an AI working as a code editor.\n\n"
           .. "Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n"
