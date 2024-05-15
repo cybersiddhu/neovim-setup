d22e0a8a6d6050b1a5c949a61c459d501b64d45c
Siddhartha Basu <biosidd@gmail.com>
2024-05-15 07:59:57 -0500

    chore(golang/lua/config): remove package-info.lua
    The package-info.lua file was removed because its only supposed to work
    with nodejs.

--- golang/lua/config/package-info.lua
+++
@@ -1,7 +0,0 @@
-require("package-info").setup()
-local utils = require "utils"
-utils.map("n","<Leader>ns","<Cmd>PackageInfoShow<CR>")
-utils.map("n","<Leader>nh","<Cmd>PackageInfoHide<CR>")
-utils.map("n","<Leader>nu","<Cmd>PackageInfoUpdate<CR>")
-utils.map("n","<Leader>ni","<Cmd>PackageInfoInstall<CR>")
-utils.map("n","<Leader>nv","<Cmd>PackageInfoChangeVersion<CR>")
