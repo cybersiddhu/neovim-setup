9495ba1e69025b4af57992c77271822684f1e715
Siddhartha Basu <biosidd@gmail.com>
2024-07-25 15:05:16 -0500

    feat(octo.lua): add suppress_missing_scope option to octo setup for projects_v2
    This change introduces a new configuration option
    `suppress_missing_scope` to the octo setup in the Terraform Lua
    configuration.

--- terraform/lua/config/octo.lua
+++ terraform/lua/config/octo.lua
@@ -1,5 +1,5 @@
-require("octo").setup({file_panel = {size = 18}})
+require("octo").setup({ file_panel = { size = 18 }, suppress_missing_scope = { projects_v2 = true } })
 local utils = require "utils"
-utils.map("n","<Leader>li","<Cmd>Octo issue list<CR>")
-utils.map("n","<Leader>lp","<Cmd>Octo pr list<CR>")
-utils.map("n","<Leader>lg","<Cmd>Octo gist list<CR>")
+utils.map("n", "<Leader>li", "<Cmd>Octo issue list<CR>")
+utils.map("n", "<Leader>lp", "<Cmd>Octo pr list<CR>")
+utils.map("n", "<Leader>lg", "<Cmd>Octo gist list<CR>")
