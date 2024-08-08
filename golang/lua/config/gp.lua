23d1a584eeb54e4246419ee6afab00b7895f8c62
Siddhartha Basu <biosidd@gmail.com>
2024-08-08 14:45:34 -0500

    feat(gp.lua): update AI model configurations and simplify disabling models
    1. Simplified the disabling of models by replacing separate `chat` and `command` flags with a single `disable` flag for clarity and ease of configuration.
    2. Added new AI model configurations for "ChatGPT4o-mini" and "CodeGPT4-mini" with specific parameters and system prompts to cater to different usage scenarios.
    3. Updated existing AI model configurations to use the newer "gpt-4o" and "gpt-4o-mini" models, ensuring the system uses the latest available technology.
    4. Refined system prompts across models to better define the role and expected behavior of the AI, enhancing user interaction and clarity in communication.

--- golang/lua/config/gp.lua
+++ golang/lua/config/gp.lua
@@ -3,30 +3,44 @@ require("gp").setup({
     -- Disable ChatGPT 3.5
     {
       name = "ChatGPT3-5",
-      chat = false,    -- just name would suffice
-      command = false, -- just name would suffice
+      disable = true,
     },
     {
       name = "CodeGPT3-5",
-      chat = false,
-      command = false,
+      disable = true,
     },
     {
-      name = "ChatGPT4",
+      name = "ChatGPT4o-mini",
       chat = true,
+      command = false,
+      -- string with model name or table with model name and parameters
+      model = { model = "gpt-4o-mini", temperature = 0.1, top_p = 1 },
+      -- system prompt (use this to specify the persona/role of the AI)
+      system_prompt = "You are a helpful assistant.\n"
+          .. "You can help me by answering my questions.\n"
+          .. "You can also ask me questions.\n",
+    },
+    {
+      name = "ChatGPT4o",
+      chat = true,
+      command = false,
+      -- string with model name or table with model name and parameters
+      model = { model = "gpt-4o", temperature = 0.1, top_p = 1 },
+      -- system prompt (use this to specify the persona/role of the AI)
+      system_prompt = "You are a helpful assistant.\n"
+          .. "You can help me by answering my questions.\n"
+          .. "You can also ask me questions.\n",
+    },
+    {
+      name = "CodeGPT4-mini",
+      chat = false,
       command = true,
       -- string with model name or table with model name and parameters
-      model = { model = "gpt-4-turbo-preview", temperature = 0.1, top_p = 1 },
+      model = { model = "gpt-4o-mini", temperature = 0.1, top_p = 1 },
       -- system prompt (use this to specify the persona/role of the AI)
-      system_prompt = "You are a general AI assistant.\n\n"
-          .. "The user provided the additional info about how they would like you to respond:\n\n"
-          .. "- If you're unsure don't guess and say you don't know instead.\n"
-          .. "- Ask question if you need clarification to provide better answer.\n"
-          .. "- Think deeply and carefully from first principles step by step.\n"
-          .. "- Zoom out first to see the big picture and then zoom in to details.\n"
-          .. "- Use Socratic method to improve your thinking and coding skills.\n"
-          .. "- Don't elide any code from your output if the answer requires coding.\n"
-          .. "- Take a deep breath; You've got this!\n",
+      system_prompt = "You are an AI working as a code editor.\n\n"
+          .. "Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n"
+          .. "START AND END YOUR ANSWER WITH:\n\n```",
     },
     {
       name = "CodeGPT4",
