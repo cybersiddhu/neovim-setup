68da0aeb238795f89f22f92d957729ea005fe639
Siddhartha Basu <biosidd@gmail.com>
2024-10-22 15:02:33 -0500

    feat(octo.lua): add suppress_missing_scope option to octo setup for improved project handling
    The configuration now includes the `suppress_missing_scope` option,
    which helps manage projects more effectively by suppressing errors
    related to missing scopes in the Octo plugin.

--- typescript/lua/config/octo.lua
+++ typescript/lua/config/octo.lua
@@ -1,4 +1,4 @@
-require("octo").setup({file_panel = {size = 18}})
+require("octo").setup({ file_panel = { size = 18 }, suppress_missing_scope = { projects_v2 = true } })
 local utils = require "utils"
 utils.map("n","<Leader>li","<Cmd>Octo issue list<CR>")
 utils.map("n","<Leader>lp","<Cmd>Octo pr list<CR>")
